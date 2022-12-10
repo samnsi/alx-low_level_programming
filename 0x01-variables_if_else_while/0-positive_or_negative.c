235b698e4c4e3824cb313e0849e1e08d278e9ffd
samnsi <royallkiing@gmail.com>
2022-12-09 21:20:15 -0800

    RAndom numbers

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -1,14 +1,25 @@
 #include <stdlib.h>
-#include <time.h>                                                                                            
+#include <time>
+#include <stdio.h>
 
-/* more headers goes there */                                                                                
-/* betty style doc for function main goes there */                                                           
-                                                                                                             
-int main(void)                                                                                               
-{                                                                                                            
-int n;                                                                                                       
-srand(time(0));                                                                                              
-n = rand() - RAND_MAX / 2;                                                                                   
-/* your code goes there */                                                                                   
-return (0);                                                                                                  
-}             
+/**
+ * main - iniiating betty
+ *
+ * Description: main
+ *
+ * Return: always 0 (sucess)
+ */
+
+int main(void)
+{
+	int n;
+	srand (time(0));
+	n =rand() - RAND_MAX / 2;
+	if (n>0)
+		printf("%d is positive\n", n);
+	else if (n == 0)
+		printf("%d is sero\n", n);
+	else
+		printf("%d is negative\n", n);
+	return (0);
+}
