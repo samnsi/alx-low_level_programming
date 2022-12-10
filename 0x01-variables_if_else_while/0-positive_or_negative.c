f1244b96b3a1a8fe29089051dd1a9524d3edfa92
samnsi <royallkiing@gmail.com>
2022-12-10 07:03:28 -0800

    Random number

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -1,27 +1,27 @@
-nclude <stdlib.h>
+#include <stdlib.h>
 #include <time.h>                         
-#include <stdio.h>                                                                                                                                                        
-                                                                                                                                                                          
-/**                                                                                                                                                                       
+#include <stdio.h>
+
+/**
+ * main - iniiating betty
  *
- *  main - iniiating betty
+ * Description: main 
  *
- * Description: main                                                                                                                                                      
- *
- * Return: always 0 (sucess)                                                                                                                                              
+ * Return: always 0 (sucess)
  *
  */                                                                                                                                                     
-                                                                                                                                                                        
-	int main(void)                                                                                                                                                  
-{                                                                                                                                                                     
-	int n;                                                                                                                                                            
-        srand (time(0));                                                                                                                                                  
-        n =rand() - RAND_MAX / 2;                                                                                                                                         
- if (n>0)                                                                                                                                                          
-        printf("%d is positive\n", n);                                                                                                                            
-        else if (n == 0)                                                                                                                                                  
+
+int main (void)
+  
+{
+  int n;
+  srand (time(0));
+  n =rand() - RAND_MAX / 2;
+  if (n>0)
+        printf("%d is positive\n", n);
+        else if (n == 0)
 	printf("%d is sero\n", n);                                                                                                                                
 	  else
-	  printf("%d is negative\n", n);                                                                                                                           
-	        return (0);                                                                                               
+	    printf("%d is negative\n", n);
+	        return (0);
 }
