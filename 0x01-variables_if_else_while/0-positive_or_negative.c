88552706766c984b934ce2051193ac21e5311a5f
samnsi <royallkiing@gmail.com>
2022-12-10 08:04:23 -0800

    Random numbers

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -1,27 +1,26 @@
-#include <stdlib.h>
-#include <time.h>                         
+#include <sdtlib.h>
+#include <time.h>
 #include <stdio.h>
 
 /**
- * main - iniiating betty
+ * main - initiating betty
  *
- * Description: main 
+ * Description: main
  *
  * Return: always 0 (sucess)
  *
- */                                                                                                                                                     
+ */
 
-int main (void)
-  
+	int main(void)
 {
-  int n;
-  srand (time(0));
-  n =rand() - RAND_MAX / 2;
-  if (n>0)
-        printf("%d is positive\n", n);
-        else if (n == 0)
-	printf("%d is sero\n", n);                                                                                                                                
-	  else
-	    printf("%d is negative\n", n);
-	        return (0);
+	int n(0);
+	srand(time(0));
+	n = rand() - RAND_MAX / 2
+	if (n > 0)
+	printf("%d is positive\n", n);
+	else if (n == 0)
+	printf("%d is sero\n", n);
+	else
+	printf("%d is negative\n", n);
+	return (0);
 }
