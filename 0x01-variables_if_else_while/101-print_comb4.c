0de19d496ac9d003c18dbb22684d82d551f3a89d
samnsi <royallkiing@gmail.com>
2022-12-10 16:37:01 -0800

    Combinations of two digits

---
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+/**
+ * main - Prints all possible combinations of two different digits,
+ *
+ * in ascending order, separated by a comma followed by a space.
+ *
+ * Return: Always 0
+ */
+
+int main(void)
+
