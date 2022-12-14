abab87b3d671b8b1ea6cbd481cc91cadce9308be
samnsi <royallkiing@gmail.com>
2022-12-14 06:46:52 -0800

    Print hours and minutes

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -5,7 +5,7 @@
  *
  * h = hour, m = minutes
  *
- * / 10 allows second digit to rotate
+ *  / 10 allows second digit to rotate
  *
  * for loop breaks before passing 24:00
  *
@@ -19,12 +19,13 @@ void jack_bauer(void)
 	for (h = 0; h < 24; h++)
 	{
 	for (m = 0; m < 60; m++)
-			{
+	{
 	_putchar((h / 10) + '0');
 	_putchar((h % 10) + '0');
 	_putchar(':');
 	_putchar((m / 10) + '0');
 	_putchar((m % 10) + '0');
 	_putchar('\n');
+	}
 	}
 }
