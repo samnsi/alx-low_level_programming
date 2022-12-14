eeb79e677d27184d06363b14db4548b57d576c49
samnsi <royallkiing@gmail.com>
2022-12-13 18:16:36 -0800

    Print lowercase

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -4,11 +4,11 @@
  *  print_alphabet - prints the alpha in lowercase
  */
 void print_alphabet(void)
-
 {
 	char ls;
 
 	for (ls = 'a'; ls <= 'z'; ls++)
 		_putchar(ls);
+
 	_putchar('\n');
 }
--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -1,11 +1,12 @@
 #ifndef MAIN_H
 #define MAIN_H
-int _putchare(char c);
+
 void print_alphabet(void);
 void print_alphabet_x10(void);
+int putchar(char c);
 int _islower(int c);
 int _isalpha(int c);
-int _isalpha(int c);
+int print_sign(int n);
 int _abs(int);
 int print_last_digit(int);
 void jack_bauer(void);
