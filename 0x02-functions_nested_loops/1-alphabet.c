4741733ee20964cb8166e2596d64e7fcf54d312d
samnsi <royallkiing@gmail.com>
2022-12-13 17:34:38 -0800

    print lowercase

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ *  print_alphabet - prints the alpha in lowercase
+ */
+void print_alphabet(void)
+
+{
+	char iv;
+
+	for (iv = 'a'; iv <= 'z'; iv++)
+	_putchar(iv);
+
+	_putchar('\n');
+}
---
+++ 0x02-functions_nested_loops/1-main.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ *
+ * main - main function of the programe
+ *
+ * Return: Always return 0"
+ */
+
+int main(void)
+
+{
+	print_alphabet();
+
+	return (0);
+}
