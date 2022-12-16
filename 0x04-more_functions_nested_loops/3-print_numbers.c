37518a15aa0c40bd77cfb07a1ec97168d40fb565
samnsi <royallkiing@gmail.com>
2022-12-16 07:07:33 -0800

    For number printing

--- 0x04-more_functions_nested_loops/3-print_numbers.c
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -6,12 +6,13 @@
  *Return: void
  */
 
-void print_numbers(void) {
+void print_numbers(void) 
+{
 	char c;
 
-	for (c = '0'; c <= '9'; c++)
+	for (c = 0; c <= 9; c++)
 	{
-	_putchar(c);
+	_putchar(c + '0');
 	}
 	_putchar('\n');
 }
