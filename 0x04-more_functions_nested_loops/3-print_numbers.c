56844896b7b93baded690e8ad7e585737bc69094
samnsi <royallkiing@gmail.com>
2022-12-16 02:57:12 -0800

    Print one or nine

--- 0x04-more_functions_nested_loops/3-print_numbers.c
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -2,12 +2,15 @@
 
 /**
  * print_numbers - a function that prints the numbers, from 0 to 9
+ *
+ * Return: 0 or 9
  */
+
 void print_numbers(void)
 {
 	int i;
 
 	for (i = 0; i <= 9; i++)
-	_putchar(i + '0');
+	_putchar('0' + i);
 	_putchar('\n');
 }
