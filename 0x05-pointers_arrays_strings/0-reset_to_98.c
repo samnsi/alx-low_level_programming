5344b9b6283f3c414fb2474f0d83a882b0cec79c
samnsi <royallkiing@gmail.com>
2022-12-19 12:42:05 -0800

    Update value to 98

--- 0x05-pointers_arrays_strings/0-reset_to_98.c
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -1,19 +1,14 @@
-#include "main.h"
-
 /**
- * main - storing the address of variable into a pointer
+ *reset_to_98 - function taking a pointer to an int as parameter
  *
- * Return: Always (0)
+ * and updates value to 98
+ *
+ *@n: input
+ *
+ *Return: n
  */
+void reset_to_98(int *n)
 
-int main(void)
 {
-	int n;
-	int *p;
-
-	n = 98;
-	p = &n;
-	_putchar("Address of 'n': %ls\n", &n);
-	_putchar("Value of 'p': %ls\n", p);
-	return (0);
+	*n = 98;
 }
