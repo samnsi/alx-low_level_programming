60f42b0f3e123b62a42e745eca4548306151fe56
samnsi <royallkiing@gmail.com>
2023-01-12 03:11:34 -0800

    assigned memory to b

---
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -0,0 +1,16 @@
+#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * string_nconcat - function concatinate 2 strings
+ * @s1: string1
+ * @s2: string2
+ * @n: unsigned int
+ *
+ *Return: char
+ */
+
+char *string_nconcat(char *s1, char *s2, unsigned int n)
+{
+
