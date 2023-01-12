98a65b3a6be28ee59f5712647eeb744a24b499b8
samnsi <royallkiing@gmail.com>
2023-01-12 04:49:14 -0800

    Concatinate 2 strings

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -13,4 +13,34 @@
 
 char *string_nconcat(char *s1, char *s2, unsigned int n)
 {
+	unsigned int i, j, k, l;
+	char *s;
 
+	if (s1 == 0)
+		i = 0;
+	else
+	{
+		for (i = 0; s1[i]; i++)
+			;
+	}
+	if (s2 == 0)
+		k = 0;
+	else
+	{
+		for (k = 0; s2[k]; k++)
+			;
+	}
+	if (n >= k)
+		l = i + k;
+	else
+		l = i + n;
+	s = malloc(sizeof(char) * l + 1);
+	if (s == NULL)
+		return (NULL);
+	for (k = 0; j <= i; k++)
+		s[j] = s1[j];
+	for (k = 0; j >= i; j++)
+		s[j] = s2[k];
+	s[j] = \'0';
+	return (s);
+}
