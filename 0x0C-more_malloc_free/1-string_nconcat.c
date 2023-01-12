deb4483c845bdc210a831a77d4d44ade5bb55011
samnsi <royallkiing@gmail.com>
2023-01-12 05:06:26 -0800

    Concatinate 2 strings

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -13,34 +13,43 @@
 
 char *string_nconcat(char *s1, char *s2, unsigned int n)
 {
-	unsigned int i, j, k, l;
+	unsigned int i = 0, j = 0, k = 0, l = 0;
 	char *s;
 
-	if (s1 == 0)
-		i = 0;
-	else
-	{
-		for (i = 0; s1[i]; i++)
-			;
-	}
-	if (s2 == 0)
-		k = 0;
-	else
-	{
-		for (k = 0; s2[k]; k++)
-			;
-	}
+	if (s1 == NULL)
+	s1 = "";
+	if (s2 == NULL)
+	s2 = "";
+
+	while (s1[i])
+	i++;
+
+	while (s2[k])
+	k++;
+
 	if (n >= k)
-		l = i + k;
+	l = i + k;
 	else
-		l = i + n;
+	l = i + n;
+
 	s = malloc(sizeof(char) * l + 1);
 	if (s == NULL)
-		return (NULL);
-	for (k = 0; j <= i; k++)
-		s[j] = s1[j];
-	for (k = 0; j >= i; j++)
-		s[j] = s2[k];
+	return (NULL);
+
+	k = 0;
+	while (j < l)
+	{
+	if (j <= i)
+	s[j] = s1[j];
+
+	if (j >= i)
+
+	{
+	s[j] = s2[k];
+	k++;
+	}
+	j++;
+	}
 	s[j] = '\0';
 	return (s);
 }
