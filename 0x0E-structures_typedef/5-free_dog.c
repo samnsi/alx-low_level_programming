f78d6a518e4286077d93dfdfd331429aa5d9aeee
samnsi <royallkiing@gmail.com>
2023-01-16 13:44:59 -0800

    free dogs

---
+++ 0x0E-structures_typedef/5-free_dog.c
@@ -0,0 +1,15 @@
+#include "dog.h"
+
+/**
+ * free_dog - frees dog
+ * @d: dogs to be freed
+ */
+
+void free_dog(dog_t *d)
+{
+	if (d == NULL)
+		return;
+	free(d->name);
+	free(d->owner);
+	free(d);
+}
