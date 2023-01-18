70df985fda516e48d8725cd198fa13aa713527c9
samnsi <royallkiing@gmail.com>
2023-01-18 04:26:46 -0800

    Print name

---
+++ 0x0F-function_pointers/0-print_name.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include "function_pointers.h"
+
+/**
+ * print_name - function that print name in function pointer.
+ * @name: input name.
+ * @f: function pointer.
+ *
+ * Return - no return
+ */
+
+void print_name(char *name, void (*f)(char *))
+{
+	if (name && f)
+		f(name);
+}
---
+++ 0x0F-function_pointers/README.md
@@ -0,0 +1,1 @@
+Function pointers - function that locate pointer to a code and/or its arguement
