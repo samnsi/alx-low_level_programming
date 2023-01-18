533722e80ae298dad36d7732f8173ffe5e7ad240
samnsi <royallkiing@gmail.com>
2023-01-18 14:52:26 -0800

    performs simple operations

--- 0x0F-function_pointers/3-main.c
+++ 0x0F-function_pointers/3-main.c
@@ -3,22 +3,18 @@
 #include "3-calc.h"
 
 /**
- * main - get_op_func has operators correlated with
- * func signs and funcs from op_func
- * if not 4 arguments, return Error & exit 98
- * if op is null, return Error & exit 99
- * if div or mod 0, return Error & exit 100
- * run calc, input i, operator, input j = pointer res to get_op
- * @argc: arguments
- * @argv: double pointer to arguments
- * Return: 0
+ * main - program that perfroms simple operations
+ * @argc: number of arguments
+ * @argv: array of arguments
+ *
+ * Return: Always 0 (Success)
  */
 
 int main(int argc, char *argv[])
 {
-	int i, j, ans;
-	int (*res)(int, int);
-	char *get_op;
+	int arg1, arg2, result;
+	char o;
+	int (*func)(int, int);
 
 	if (argc != 4)
 	{
@@ -26,27 +22,28 @@ int main(int argc, char *argv[])
 		exit(98);
 	}
 
-	i = atoi(argv[1]);
-	j = atoi(argv[3]);
-	get_op = argv[2];
+	arg1 = atoi(argv[1]);
+	arg2 = atoi(argv[3]);
 
-	/* added edge case if argv[2] was longer than 1 char */
-	if (get_op_func(argv[2]) == NULL || argv[2][1] != '\0')
+	func = get_op_func(argv[2]);
+
+	if (!func)
 	{
 		printf("Error\n");
 		exit(99);
 	}
 
-	if ((*get_op == '/' || *get_op == '%') && (*argv[3] == '0'))
+	o = *argv[2];
+
+	if ((o == '/' || o == '%') && arg2 == 0)
 	{
 		printf("Error\n");
 		exit(100);
 	}
 
-	res = get_op_func(get_op);
-	ans = res(i, j);
+	result = func(arg1, arg2);
 
-	printf("%d\n", ans);
+	printf("%d\n", result);
+
 	return (0);
 }
-
