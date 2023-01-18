168afe08a99a76ff97aebe1c215a87b0b808ff35
samnsi <royallkiing@gmail.com>
2023-01-18 15:01:01 -0800

    performs simple operations

--- 0x0F-function_pointers/3-get_op_func.c
+++ 0x0F-function_pointers/3-get_op_func.c
@@ -1,31 +1,31 @@
-#include <stdio.h>
-#include <stdlib.h>
-#include "3-calc.h"
-
 /**
- * get_op_func - perform the operation asked by the user
- * @s: char operator
+ * get_op_func - function pointer that selects the correct function to perform
+ * the operation asked by the user
+ * @s: the operator given by the user
  *
- * Return: pointer to the function that correspond to the operator
+ * Return: pointer to the function that corresponds to the
+ * operator given as a parameter
  */
-
 int (*get_op_func(char *s))(int, int)
 {
 	op_t ops[] = {
-	{"+", op_add},
-	{"-", op_sub},
-	{"*", op_mul},
-	{"/", op_div},
-	{"%", op_mod},
-	{NULL, NULL}
+		{"+", op_add},
+		{"-", op_sub},
+		{"*", op_mul},
+		{"/", op_div},
+		{"%", op_mod},
+		{NULL, NULL}
 	};
-	int i = 0;
+	int i;
 
-	while (i < 10)
+	i = 0;
+
+	while (ops[i].op)
 	{
-	if (s[0] == ops->op[i])
-	break;
-	i++;
+		if (strcmp(ops[i].op, s) == 0)
+			return (ops[i].f);
+		i++;
 	}
-	return (ops[i / 2].f);
+
+	return (NULL);
 }
